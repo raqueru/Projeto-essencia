f3da44ccd3883cd369d5f7d9e16cd4e4223c91ff
raks <raquelolho@hotmail.com>
2020-09-09 19:25:12 -0300

    codigos de andar, do escudo e da camera

---
+++ Projeto essencia/Assets/Scripts/MovimentoCamera.cs
@@ -0,0 +1,30 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimentoCamera : MonoBehaviour
+{
+    [Header("Referências do Unity. Cuidado ao mexer")]
+    [Tooltip("alvo que a camera segue (Prefere-se que seja o jogador)")]
+    public GameObject Alvo;
+    [Tooltip("Velocidade de movimento da camera. Quanto mais próxima à do alvo, mais centralizado ele fica. Deve ser positiva")]
+    public float moveSpeed;
+
+    [Tooltip("Posição relativa entre camera e alvo. Mantenha um Z negativo")]
+    public Vector3 offset;
+    private Transform transfPlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transfPlayer = Alvo.GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        //A: move-se junto com o player 
+        transform.position = Vector3.Lerp(transform.position, transfPlayer.position + new Vector3(0,0,-10), moveSpeed * Time.deltaTime);
+    }
+}
---
+++ Projeto essencia/Assets/Scripts/ShieldMov.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShieldMov : MonoBehaviour
+{
+    public GameObject player;
+    public float fRadius; //distancia do escudo pro player
+    private Transform pivot;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = FindObjectOfType<PlayerMov>().gameObject;
+        pivot = new GameObject().transform;
+        transform.parent = pivot;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector3 v3Pos = Camera.main.WorldToScreenPoint(player.transform.position); //pega a posicao do player no mundo
+        v3Pos = Input.mousePosition - v3Pos;//garante que vai ser sempre a msm distancia do player independente da distancia do mouse
+        float angle = Mathf.Atan2(v3Pos.y, v3Pos.x) * Mathf.Rad2Deg;
+        v3Pos = Quaternion.AngleAxis(angle, Vector3.forward) * (Vector3.right * fRadius);//mexe o escudo pra direcao do mouse
+        transform.position = player.transform.position + v3Pos;
+        transform.right = player.transform.position - transform.position;//rotaciona sempre o escudo em direcao ao player
+    }
+
+}
