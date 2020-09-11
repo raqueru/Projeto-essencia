735977e1c8a084a1b108b63bc6a56128e9cb068b
raks <raquelolho@hotmail.com>
2020-09-11 12:22:16 -0300

    Commit das bullets e do inimigo em progresso

---
+++ Projeto essencia/Assets/Scripts/BulleScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulleScript : MonoBehaviour
+{
+    [Header("Shoot Configuration")]
+
+    public GameObject Bullet;
+    private GameObject Player;
+
+
+    public float Strenght = 20;
+
+    void Start(){
+          Player= GameObject.FindGameObjectWithTag("Player");
+    }
+    private void Update()
+    {
+        Vector2 direction = (Player.transform.position - this.transform.position).normalized;
+        Shoot(direction);
+    
+    }
+
+    public void Shoot(Vector2 direction)
+    {
+
+        GameObject bullet = Instantiate(Bullet, this.transform.position, Bullet.transform.rotation);
+
+
+
+        Rigidbody2D bulletRB = bullet.GetComponent<Rigidbody2D>();
+
+        bulletRB.AddForce(direction * Strenght, ForceMode2D.Impulse);
+         
+
+    }
+
+}
---
+++ Projeto essencia/Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{ 
+    private GameObject Player;
+    public float Speed = 30;
+    public float FollowDistance = 100f;
+
+ 
+
+
+
+    private void Start()
+    {
+        Player= GameObject.FindGameObjectWithTag("Player");
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+         
+        if (Vector2.Distance(Player.transform.position, this.transform.position) < FollowDistance)
+        {
+            walktowardsplayer();
+        }
+        
+    }
+
+    void walktowardsplayer()
+    { 
+        transform.position=Vector2.MoveTowards(transform.position,Player.transform.position,Speed*Time.deltaTime);
+     
+        if(Player.transform.position.x<this.transform.position.x){
+            transform.rotation= new Quaternion(0,180,0,0);
+        }
+        else transform.rotation= new Quaternion(0,0,0,0);
+
+        
+    }
+
+    private static EnemyScript instance;
+    public static EnemyScript GetInstance()
+    {
+        return instance;
+    }
+
+}
